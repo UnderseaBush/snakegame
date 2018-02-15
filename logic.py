989bce68ce3e1a672c09014d02bfbcec50ddfc0d
mtumelty <m186606@usna.edu>
2018-02-15 11:28:11 -0500

    lets collab

--- logic.py
+++ logic.py
@@ -2,16 +2,20 @@
 import pygame, sys
 import serial
 from pygame.locals import *
+import time
 #import snake
 #import food
 
 WinWIDTH = 500
 WinHEIGHT = 480
 
+FPS = 30
+
 GREY = (128, 128, 128)
+RED = (255, 0, 0)
 
 def main():
-	pygame.init
+	pygame.init()
 	fpsClock = pygame.time.Clock()
 	window = pygame.display.set_mode((WinWIDTH, WinHEIGHT), 0, 32)
 	pygame.display.set_caption('SNAKE')
@@ -23,6 +27,8 @@ def main():
 			#kEY events
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_UP:
+					print 'up arrow'
+					Message(window, 'GAME START')
 					#TODO remaining key events
 					
 			#QUIT event
@@ -33,9 +39,20 @@ def main():
 		#Update Display
 		drawWorld(window)
 		pygame.display.update()
+		fpsClock.tick(FPS)
 		
 def drawWorld(surf):
 	surf.fill(GREY)
+	
+def Message(surf, msg):
+	#display GAME OVER and WINNER
+	fontObj = pygame.font.Font('freesansbold.ttf', 32)
+	textSurfObj = fontObj.render(msg, True, RED)
+	textRectObj = textSurfObj.get_rect()
+	textRectObj.center = (WinWIDTH/2, WinHEIGHT/2)
+	surf.blit(textSurfObj,textRectObj)
+	pygame.display.update()
+	time.sleep(1)
 
 if __name__=="__main__":
 	main()
