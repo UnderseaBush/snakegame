c3507d7b27f6cd53e943173e843e9f6c71d776b9
Bush <mtbush118@gmail.com>
2018-02-22 10:52:01 -0500

    hope this works

--- food.py
+++ food.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
 
+
 import pygame
+FOOD_SIZE = 10
+FOOD_COLOR = (255,0,0)
 
 class Food:
-	def __init__(self,x,y)
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+		self.rect =  pygame.Rect((0,0,FOOD_SIZE,FOOD_SIZE))
 		self.moveTo(x,y)
 
-	def move(self, x, y)
+	def move(self, x, y):
 		self.x = x
 		self.y = y
 	
-	def draw(self,surf)
+	def draw(self,surf):
+		
+		self.rect.center = (self.x,self.y)
+		pygame.draw.rect(surf, FOOD_COLOR, self.rect)
+
+	def hitBy(self,rect):
+		return self.rect.colliderect(rect)
 		
 		
--- snake.py
+++ snake.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python
+import pygame
+SNAKE_SIZE = 10
+SNAKE_COLOR = (0,0,255)
+
+
+Class Snake:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.rect = pygame.Rect((0,0,SNAKE_SIZE,SNAKE_SIZE))
+		self.vel = 1 
+
+	def moveUp(self, dt):
+		self.y = self.y - self.vel * dt
+
+	def moveDown(self, dt):
+		self.y = self.y + self.vel * dt
+
+	def moveLeft(self, dt):
+		self.x = self.x-self.vel * dt
+	
+	def moveRight(self, dt):
+		self.x = self.x+ self.vel *dt
+		
+
+	def draw(self, surf):
+		self.rect.center = (self.x,self.y)
+		pygame.draw.rect(surf, SNAKE_COLOR, self.rect)
