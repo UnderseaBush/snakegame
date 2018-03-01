6cc2a81eb6b518427cf6ff784f661e5a995ef630
Bush <mtbush118@gmail.com>
2018-03-01 11:20:12 -0500

    trying to pull

--- snake.py
+++ snake.py
@@ -8,25 +8,21 @@ class Snake:
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
-		self.v_x = 1
 		self.rect = pygame.Rect((0,0,SNAKE_SIZE,SNAKE_SIZE))
-		self.vel = 1
-
-	def move(self, time):
-		self.x = self.x + self.v_x*time
+		self.vel = 1 
 
 	def moveUp(self, dt):
 		self.y = self.y - self.vel * dt
 
 	def moveDown(self, dt):
-		self.y = self.y + self.vel * (-dt)
+		self.y = self.y + self.vel * dt
 
 	def moveLeft(self, dt):
-		self.x = self.x-self.vel * (-dt)
-
+		self.x = self.x-self.vel * dt
+	
 	def moveRight(self, dt):
 		self.x = self.x+ self.vel *dt
-
+		
 
 	def draw(self, surf):
 		self.rect.center = (self.x,self.y)
